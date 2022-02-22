d60e9bae684efc8f37024070a8ac5b1f394af7d9
Michael Champlin <mchamplin@sbcglobal.net>
2022-02-21 22:18:06 -0800

    First during-class update

--- public/app.js
+++ public/app.js
@@ -1,46 +1,118 @@
 // Create map:                                                       
-const myMap = L.map('map', {
-    center: [48.868672, 2.342130],
-    zoom: 12,
-});
 
-// Add OpenStreetMap tiles:
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-}).addTo(myMap)
+const targetMap = {
+	coordinates: [],
+	map: {}, 
+	markers: {},
 
-// Create and add a geolocation marker:
-const marker = L.marker([48.87007, 2.346453])
-marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
+	// build leaflet map
+	showMap() {
+		this.map = L.map('map', {
+            center: this.coordinates,
+            zoom: 13,
+            });
 
-// Draw the 2nd Arrondissement  
-var polygon = L.polygon([                                     
-    [48.863368120198004, 2.3509079846928516],
-    [48.86933262048345, 2.3542531602919805],
-    [48.87199261164275, 2.3400569901592183],
-    [48.86993336274516, 2.3280142476578813],
-    [48.86834104280146, 2.330308418109664]
-    ],{fill:false}).addTo(myMap);
-    
+		// add openstreetmap tiles
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(this.map)
 
-var myIcon = L.icon({
-    iconUrl: './assets/red-pin.png',
-    iconSize: [38, 38],
-    iconAnchor: [19, 38],
-    popupAnchor: [-3, -76],
-});
+		// create and add geolocation marker
+		const marker = L.marker(this.coordinates)
+            marker
+            .addTo(this.map)
+            .bindPopup('<p1><b>Your current location</b><br></p1>')
+            .openPopup()
+	},
+
+    addOneMarker(lat_long, biz_name) {
+        this.markers = L.marker(lat_long)
+			.bindPopup(`<p1>${biz_name}</p1>`)
+			.addTo(this.map)
+    },
+
+	// add business markers
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) 
+            this.addOneMarker([this.businesses[i].lat, this.businesses[i].long], this.businesses[i].name)
+	},
+
+    showBusinesses(business_group) {
+        let businesses = business_group.map((biz) => {
+            let site = {
+                name: biz.name,
+                lat: biz.geocodes.main.latitude,
+                long: biz.geocodes.main.longitude
+            };
+            targetMap.addOneMarker([site.lat, site.long], site.name)
+            
+            console.log(site)
+        })
+    }
+}
+
+async function getCoords(){
+	const pos = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [pos.coords.latitude, pos.coords.longitude]
+}
+
+window.onload = async () => {
+	const coords = await getCoords()
+	targetMap.coordinates = coords
+	targetMap.showMap()
+    console.log("My location: " + targetMap.coordinates)
+}
+
+async function getFoursquare(bizCategory) {
+    const options = {
+        method: 'GET',
+        headers: {
+        Accept: 'application/json',
+        Authorization: 'fsq3bGMbnnDlsN4TQJUVXNSjgs7V9tw4Kfe3M0pgG0ol67U='
+        }
+    }
+    let latitude = targetMap.coordinates[0]
+    let longitude = targetMap.coordinates[1]
+    let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${bizCategory}&limit=5&ll=${latitude}%2C${longitude}`, options)
+
+    let result = await response.text()
+    let parsedResults = JSON.parse(result)
+    let businesses = parsedResults.results
+    console.log(businesses)
+    return businesses
+}
+
+async function getCoffeeShops() {
+    console.log("Find coffee")
+    let businessGroup = await getFoursquare('Coffee')
+    targetMap.showBusinesses(businessGroup)  
+}
+async function getRestaurants() {
+    console.log("Find Restaurants")
+    let businessGroup = await getFoursquare('Restaurants')
+    targetMap.showBusinesses(businessGroup)  
+}
+async function getHotels() {
+    console.log("Find Hotels")
+    let businessGroup = await getFoursquare('Hotel')
+    targetMap.showBusinesses(businessGroup)  
+}
+async function getMarkets() {
+    console.log("Find Markets")
+    let businessGroup = await getFoursquare('Restaurants')
+    targetMap.showBusinesses(businessGroup)  
+}
+
+document.getElementById("b1").addEventListener("click", function(){ getCoffeeShops() })
+document.getElementById("b2").addEventListener("click", function(){ getRestaurants() })
+document.getElementById("b3").addEventListener("click", function(){ getHotels() })
+document.getElementById("b4").addEventListener("click", function(){ getMarkets() })
+
+//fsq3bGMbnnDlsN4TQJUVXNSjgs7V9tw4Kfe3M0pgG0ol67U=
 
 
-    // Metro station markers:
-const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: myIcon}).bindPopup('Réaumur-Sébastopol')
-const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: myIcon}).bindPopup('Strasbourg-Saint-Denis')
-const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
-const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
-const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
-const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
-
-const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
-// create red pin marker
 
 
